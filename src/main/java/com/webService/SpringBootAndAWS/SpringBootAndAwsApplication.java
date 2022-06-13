7ff3ae92dd6c8175d5d81139efbafb4c4fbda7ed
ByeoungJoon Jeon <juni8453@naver.com>
2022-06-13 19:36:47 +0900

    Initial commit

---
+++ src/main/java/com/webService/SpringBootAndAWS/SpringBootAndAwsApplication.java
@@ -0,0 +1,13 @@
+package com.webService.SpringBootAndAWS;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootAndAwsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootAndAwsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/webService/SpringBootAndAWS/SpringBootAndAwsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.webService.SpringBootAndAWS;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootAndAwsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
