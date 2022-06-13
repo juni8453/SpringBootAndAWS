5173672beebf0283bc75325bf90f4b2470a87018
ByeoungJoon Jeon <juni8453@naver.com>
2022-06-13 21:29:19 +0900

    :white_check_mark: test: 게시글 등록 API 테스트 작성
    1. HelloControllerTest 와 달리 WebMvcTest 사용 X
      - JPA 기능이 작동하지 않기 때문
      - Controller, ControllerAdvice 등 외부 연동과 관련된 부분만 활성화
    
    2. 따라서 JPA 기능까지 한번에 테스트할 때는 @SpringBootTest 를 통해 포트 설정을 하고 restTemplate 를 사용

---
+++ src/test/java/com/webService/SpringBootAndAWS/web/PostsApiControllerTest.java
@@ -0,0 +1,67 @@
+package com.webService.SpringBootAndAWS.web;
+
+import com.webService.SpringBootAndAWS.domain.posts.Posts;
+import com.webService.SpringBootAndAWS.domain.posts.PostsRepository;
+import com.webService.SpringBootAndAWS.web.dto.PostsSaveRequestDto;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.web.client.TestRestTemplate;
+import org.springframework.boot.test.web.server.LocalServerPort;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+
+import java.util.List;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+// 아래처럼 랜덤 포트 설정을 해줘야 restTemplate 가 동작한다.
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+class PostsApiControllerTest {
+
+    @LocalServerPort
+    private int port;
+
+    @Autowired
+    private TestRestTemplate restTemplate;
+
+    @Autowired
+    private PostsRepository postsRepository;
+
+    @AfterEach
+    public void tearDown() throws Exception {
+        postsRepository.deleteAll();
+    }
+
+    /* 왜 HelloController 와 달리 @WebMvcTest 를 사용하지 않았는가 ?
+    *  @WebMvcTest 의 경우, JPA 기능이 작동하지 않기 때문 !
+    *  아래 처럼 JPA 기능을 한꺼번에 테스트하기 위해선, @SpringBootTest, TestRestTemplate 를 사용하면 된다.
+    * */
+    @Test
+    @DisplayName("게시글을 등록하는 테스트입니다.")
+    void save() throws Exception {
+
+        String title = "title";
+        String content = "content";
+
+        PostsSaveRequestDto requestDto = PostsSaveRequestDto.builder()
+                .title(title)
+                .content(content)
+                .author("tany@naver.com")
+                .build();
+
+        String url = "http://localhost:" + port + "/api/v1/posts";
+
+        ResponseEntity<Long> responseEntity = restTemplate.postForEntity(url, requestDto, Long.class);
+
+        assertThat(responseEntity.getStatusCode()).isEqualTo(HttpStatus.OK);
+        assertThat(responseEntity.getBody()).isGreaterThan(0L);
+
+        List<Posts> all = postsRepository.findAll();
+
+        assertThat(all.get(0).getTitle()).isEqualTo(title);
+        assertThat(all.get(0).getContent()).isEqualTo(content);
+    }
+}
